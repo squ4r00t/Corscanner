55a56c9586b22d03269a8c4675607b40e97fabb8
amxdou00 <amxdou.00@proton.me>
2024-10-27 04:00:14 +0100

    first

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Corscanner
+## Corscanner
+___
+This tool helps you identify CORS misconfiguration in a website.
+
+### Installation
+___
+
+```bash
+git clone https://github.com/squ4r00t/Corscanner.git
+cd Corscanner
+python3 main.py -h
+```
+
+### Usage
+___
+
+**Basic Usage**
+```bash
+python3 main.py --url https://example.com/vulnerable/endpoint
+```
+
+If you know a valid origin that is accepted by the application you're testing, you can pass it to the tool with the `--valid-origin` parameter
+
+> When using `--valid-origin`, provide the domain name without the scheme (http, https)
+
+**With valid origin**
+```bash
+python3 main.py --url https://example.com/vulnerable/endpoint --valid-origin www.example.com
+```
+
+If you want to test multiple urls, you can give the tool the path to the file containing the list of urls through the `--file` parameter.
+
+In this case, if you want to also provide a valid origin for each url, do it appending to each of them a comma (,) and the valid origin. Like this
+
+```text
+https://site1.com/vulnerable/endpoint,site1.com
+https://site2.com/another/endpoint,www.site2.com
+```
+
+**Using a list of urls**
+```bash
+python3 main.py --file /opt/urls.txt
+```
---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+import argparse
+import os
+
+# Define color variables
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# CORS headers
+ACAO_STR = "access-control-allow-origin"
+ACAC_STR = "access-control-allow-credentials"
+
+def test_url(url, valid_origin):
+    print(f"{YELLOW}[i] Url: {url}{RESET}")
+    payloads = ["https://evil.com", "null"]
+
+    if valid_origin != "":
+        payloads.append(f"https://{valid_origin}")
+        payloads.append(f"https://{valid_origin}.evil.com")
+        payloads.append(f"https://evil{valid_origin}")
+        payloads.append(f"https://evil.com/{valid_origin}")
+
+    for payload in payloads:
+        print(f"{BLUE}[i] Testing {payload}{RESET}")
+        headers = {
+            'Origin': payload,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 GLS/100.10.9939.100'
+        }
+        response = requests.get(url, headers=headers)
+
+        # Checking CORS headers in response
+        if ACAO_STR in response.headers and ACAC_STR in response.headers:
+            if response.headers.get(ACAO_STR) == payload and response.headers.get(ACAC_STR) == "true":
+                print(f"{GREEN}[+] [{response.status_code}] - {url} is vulnerable to the payload -> Origin: {payload}{RESET}")
+                print(f"{GREEN}[+] Access-Control-Allow-Origin: {response.headers.get(ACAO_STR)}{RESET}")
+                print(f"{GREEN}[+] Access-Control-Allow-Credentials: {response.headers.get(ACAC_STR)}{RESET}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Detects CORS misconfigurations in a URL or a list of URLs")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--url", type=str, help="A single URL to process.")
+    group.add_argument("--file", type=str, help="A path to a file containing URLs.")
+
+    parser.add_argument("--valid-origin", type=str, help="A valid origin that is accepted by the server")
+
+    args = parser.parse_args()
+
+    if args.file:
+        if not os.path.isfile(args.file):
+            print(f"{RED}[-] The file '{args.file}' does not exist.{RESET}")
+            exit(1)
+
+    if args.url:
+        print(f"{MAGENTA}[!] Better be a valid url, didn't check for it...{RESET}")
+        if args.valid_origin:
+            test_url(args.url, args.valid_origin)
+        else:
+            test_url(args.url, "")
+    elif args.file:
+        print(f"501 - Not implemented, coming soon...")
